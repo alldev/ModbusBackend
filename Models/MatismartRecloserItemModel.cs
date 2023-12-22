a3d748eda14159b44a1b64a2bd0ab25d86cd524f
Abraham Denver <123178078+alldev@users.noreply.github.com>
2023-12-22 13:12:47 +0500

    v.0.0.1 - Model & Repository for Matismart devices

---
+++ Models/MatismartRecloserItemModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModbusBackend.Models
+{
+    public enum RecloserType
+    {
+        MCB, // Miniature Circuit Breaker
+        RCB, // Residual Current Device
+    }
+
+    public enum RecloserControlOver
+    {
+        Light,
+        Sockets,
+        Line
+    }
+
+    public class MatismartRecloserItemModel
+    {
+        public byte SlaveId;
+        public string Comment;
+        public RecloserType Type;
+        public RecloserControlOver ControlOver;
+        public MatismartRecloserItemModel? Parent;
+        public byte GroupID;
+
+        public MatismartRecloserItemModel(byte slaveId, string comment, MatismartRecloserItemModel? parent,
+            RecloserType type, RecloserControlOver controlOver, byte groupID)
+        {
+            SlaveId = slaveId;
+            Comment = comment;
+            Parent = parent;
+            Type = type;
+            ControlOver = controlOver;
+            GroupID = groupID;
+        }
+    }
+}
---
+++ Repository/MatismartItemsRepository.cs
@@ -0,0 +1,19 @@
+﻿using ModbusBackend.Models;
+
+namespace ModbusBackend.Repository
+{
+    // TODO: create abstract class and move this to it's implementation
+    public class MatismartItemsRepository
+    {
+        public readonly List<MatismartRecloserItemModel> items = new();
+        public MatismartItemsRepository()
+        {
+            var item = new MatismartRecloserItemModel(118, "test item 1", null, RecloserType.MCB, RecloserControlOver.Light, 0);
+            items.Add(item);
+            item = new MatismartRecloserItemModel(118, "test item 2", null, RecloserType.MCB, RecloserControlOver.Light, 0);
+            items.Add(item);
+            item = new MatismartRecloserItemModel(118, "test item 3", null, RecloserType.MCB, RecloserControlOver.Light, 0);
+            items.Add(item);
+        }
+    }
+}
